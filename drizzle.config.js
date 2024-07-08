77900558b0ef02d63e021058b76819f700d73588
ArshiyaArora <aroraarshiya2004@gmail.com>
2024-07-09 02:17:27 +0530

    Backend Setup(Drizzle ORM + PostgreeSQL

---
+++ drizzle.config.js
@@ -0,0 +1,9 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+    schema: "./utils/schema.js",
+    dialect: 'postgresql',
+    dbCredentials: {
+      url:'postgresql://NEXT.js%20project_owner:d13RictkBosY@ep-cold-mountain-a5lkrobs.us-east-2.aws.neon.tech/NEXT.js%20project?sslmode=require',
+    }
+  };
+  
