b8fffde9f4f872ecf0f2ef9726b9f8acc03782b6
ArshiyaArora <aroraarshiya2004@gmail.com>
2024-07-10 15:25:29 +0530

    Generate Mock Interview Question With AI

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,7 +3,7 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-      url:'postgresql://NEXT.js%20project_owner:d13RictkBosY@ep-cold-mountain-a5lkrobs.us-east-2.aws.neon.tech/NEXT.js%20project?sslmode=require',
+      url:'postgresql://ai-interview-mocker_owner:ndJat08HpWXj@ep-divine-limit-a5lyq1hv.us-east-2.aws.neon.tech/ai-interview-mocker?sslmode=require',
     }
   };
   
